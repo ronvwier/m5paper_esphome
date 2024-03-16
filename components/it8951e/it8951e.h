3d759412afb21ec9e6873f19dd56e33c8f6e721c
Ron van Wier <61551255+ronvwier@users.noreply.github.com>
2024-03-16 13:23:02 +0100

    Update it8951e.h

--- components/it8951e/it8951e.h
+++ components/it8951e/it8951e.h
@@ -8,6 +8,7 @@ namespace esphome {
 namespace it8951e {
 
 class IT8951ESensor : // public PollingComponent,
+                      public Component,
                       public display::DisplayBuffer,
                       public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING,
                                             spi::DATA_RATE_20MHZ> {
